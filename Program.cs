897b322a3edb29a4e2f8e0d10b9232c740a527e8
Anna Kosheleva <mrs_kosheleva@inbox.ru>
2023-01-13 15:34:57 +0300

    sixth

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int z) {
+    string[] output = new string[CountLessThan(input, z)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= z) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int z) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= z) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray() {
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
